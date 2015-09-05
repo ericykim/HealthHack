e8c1231dcae8f2e7fa2fac9f6fa149d25431f6b5
Salman <salmanfakhri2@gmail.com>
2015-09-05 14:54:35 -0400

    routing

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+var app = angular.module('starter', ['ionic'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -18,3 +18,11 @@ angular.module('starter', ['ionic'])
   });
 })
 
+app.config(function($stateProvider, $urlRouterProvider) {
+  $urlRouterProvider.otherwise('/')
+
+  $stateProvider.state('home', {
+    url: '/',
+    template: 'home.html'
+  })
+})
